2656968cdfa50e501186ba15d4aa753340e80f8e
Youssif Mahmed <youssifm.raghib@gmail.com>
2025-04-11 20:50:31 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YoussifMahmed/malicious_package1
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package malicious_package1
+
+import "fmt"
+
+func init() {
+    fmt.Println("✅ Code executed via go get!")
+}
